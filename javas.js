1853f3c5309816892dd9d938b1e642388ac0b67e
bobodokey <94241624+Bodokeyk@users.noreply.github.com>
2025-06-01 19:39:56 -0600

    add basic code for create and add a book object to the array

--- javas.js
+++ javas.js
@@ -1,9 +1,18 @@
+const bookLibrary = []
+
 function Book(name,pages,read){
     this.name = name;
     this.pages = pages;
     this.read = function() {return read ? "Read":"not read yet";}
-    this.info = this.name + " " + this.pages + " " + this.read();
-  }
-  let potter = new Book("harry photer","240",true)
-  console.log(potter.info);
+  };
+
+  function addToLibrary(nameTo,pagesTo,readTo){
+    let bookCreated = new Book(nameTo,pagesTo,readTo)
+    bookCreated.newId = crypto.randomUUID()
+    bookLibrary.push(bookCreated)
+  };
+
+  addToLibrary("Harry Photer",250,true)
+  console.log(bookLibrary)
   
+  console.log("end of code")
