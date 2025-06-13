8d9c2cab21c6c6ccaf516bbdef01edcf7a51c09b
bobodokey <94241624+Bodokeyk@users.noreply.github.com>
2025-06-12 19:26:49 -0600

    add javascript to read out the array filled with books and set them up on the html doc, still need to review some notes on it and keep working it out

--- javas.js
+++ javas.js
@@ -3,33 +3,47 @@ const grid_Holder = document.querySelector('.Grid-Holder');
 const navBar_Holder = document.querySelector('.NavBar-Holder')
 const bookLibrary = [];
 
-function Book(name,pages,read){
+function Book(url,name,pages,read){
+    this.url = url
     this.name = name;
     this.pages = pages;
-    this.read = function() {return read ? "Read":"Not read yet";}
+    const readed = () => {return read ? "Read":"Not read yet";}/* 
+    Dont know how this worked :p  */
+    this.read = readed()
   };
 
-  function addToLibrary(nameTo,pagesTo,readTo){
+  function addToLibrary(urlTo,nameTo,pagesTo,readTo){
     if(bookLibrary.length >= 10){
       return alert("Max books reached")
     }
-    const bookCreated = new Book(nameTo,pagesTo,readTo)
-    bookCreated.newId = crypto.randomUUID()
+    const bookCreated = new Book(urlTo,nameTo,pagesTo,readTo)
     bookLibrary.push(bookCreated)
   };
 
-  function create_Book_object(){
+
+
+  
+  function create_Book_object(arrayWithProperties){
     const mainDiv = document.createElement("div");
     mainDiv.classList = "Book-Object";
-    mainDiv.appendChild(document.createElement("img"));
-
-    for(let i = 1; i <= 3; i++){
-      mainDiv.appendChild(document.createElement("p"))
-    }
+    arrayWithProperties.forEach(element => {
+      if(mainDiv.lastChild === null){
+        mainDiv.appendChild(document.createElement("img"));
+        mainDiv.lastElementChild.setAttribute("src", element)
+      }else{
+        mainDiv.appendChild(document.createElement("p"))
+        mainDiv.lastElementChild.innerHTML = element
+      }
+      
+    });
+    
     grid_Holder.appendChild(mainDiv);
   }
 
   function create_Book_holder(){
+    if(navBar_Holder.firstElementChild !== null){
+      return
+    }
     const mainDiv = document.createElement("div");
     mainDiv.classList = "Book-Holder";
     mainDiv.appendChild(document.createElement("img"));
@@ -40,44 +54,21 @@ function Book(name,pages,read){
     navBar_Holder.appendChild(mainDiv);
     }
 
-    
-
-/* pseudo code
-
-  *needs
-  -function to create the Book-Object template /
-  -function to create the Book-Holder template
-
-  -function to go trought the array
-  -function to get the data of the object on at the moment
-  
-  -function to set the data to te Book-Holder when clicked the book
-  -function to set object data to a Book-Object
-  -function to clear all the Grid-Holder?
-  -function to clear the NavBar-Holder?
 
 
-  +parameters to take care of
-  -for the moment no more than 10 book objects
-  -create first the books objects and then set the data 
-  or create the book, set the data and then the next one 
-  -i dont have images data yet
-  -
 
+    function trought_array(arrayToGoThought){
+      create_Book_holder()
 
-*/
-  create_Book_object()
-  create_Book_holder()
-/* 
-  while(bookLibrary.length <= 9){
-    addToLibrary("Harry Photer",250,true)
-  console.log(bookLibrary)
-  
-  console.log("end of code")
-  } */
+      arrayToGoThought.forEach(element => {
+        create_Book_object(Object.values(element))
+        element.newId = crypto.randomUUID() /* creating it here but not 
+        ideal...might review it later */
+      });
+    }
 
-  /* fixes
-  -minor fix to overflow now to be hidden so there wont be
-  any color issues for the Grid-Holder
-  -minor fix to prevent the array holder to be over 10 objects
-  */
+/* educational purpose only and repetition */
+    for (let index = 1; index <= 10; index++) {
+      addToLibrary("nada","harry photer" + index,256,true)
+    }
+  trought_array(bookLibrary)
